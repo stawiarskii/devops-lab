791ba0338d7d01624f1fde5e2f79bd39eca99e21
MSR User <msr@example.com>
2026-01-06 14:48:27 +0000

    Update to version 2.0

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Witaj! To moja aplikacja Python w kontenerze Nginx.</h1>"
+    return "<h1>Wersja 2.0 - CI/CD działa automatycznie!</h1>"
 
 if __name__ == '__main__':
     # Uruchamiamy na porcie 8080 i na wszystkich interfejsach (0.0.0.0)
