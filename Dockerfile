2b4e305de9b9e87eb5cb41c3edcadd224b942420
Maciek Stawiarski <maciej.stawiarski7@gmail.com>
2026-04-27 18:56:14 +0200

    Baza danych: Włączenie trybu AOF w Redisie.

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,11 @@
-# Zmieniamy bazę na wersję Alpine (lżejsza i szybsza)
 FROM nginx:alpine
 
-# W Alpine używamy 'apk' zamiast 'apt-get'
-# Instalujemy Pythona i PIPa
 RUN apk add --no-cache python3 py3-pip
 
-# Tworzymy środowisko wirtualne (tak jak poprzednio)
 ENV VIRTUAL_ENV=/opt/venv
 RUN python3 -m venv $VIRTUAL_ENV
 ENV PATH="$VIRTUAL_ENV/bin:$PATH"
 
-# Reszta bez zmian
 WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -3,18 +3,14 @@ import redis
 
 app = Flask(__name__)
 
-# Nawiązujemy połączenie z bazą Redis
-# Zauważ, że host to po prostu słowo 'redis', a nie adres IP!
 cache = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
     try:
-        # Próbujemy zwiększyć licznik o nazwie 'hits' w bazie
         count = cache.incr('hits')
         return f"Witaj na produkcji! Ta strona zostala odwiedzona {count} razy.\n"
     except redis.exceptions.ConnectionError:
-        # Ten blok wykona się, jeśli baza Redis będzie wyłączona
         return "Witaj! Aplikacja dziala, ale nie moze polaczyc sie z baza Redis.\n"
 
 if __name__ == '__main__':
