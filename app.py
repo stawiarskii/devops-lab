1790a08fc85b07972efa062fe8aef4f5969b0653
Maciek Stawiarski <maciej.stawiarski7@gmail.com>
2026-05-08 20:39:10 +0200

    Poprawki formatowania PEP 8

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 cache = redis.Redis(host='redis', port=6379)
 
+
 @app.route('/')
 def hello():
     try:
@@ -12,6 +13,7 @@ def hello():
         return f"Witaj na produkcji! Ta strona zostala odwiedzona {count} razy.\n"
     except redis.exceptions.ConnectionError:
         return "Witaj! Aplikacja dziala, ale nie moze polaczyc sie z baza Redis.\n"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
